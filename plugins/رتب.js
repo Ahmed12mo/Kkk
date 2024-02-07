d364ca5c616031391c8607fc73a2ee2a5b531a6c
Kyutaka101 <142097798+Kyutaka101@users.noreply.github.com>
2024-02-07 16:15:21 +0100

    Update رتب.js

--- plugins/رتب.js
+++ plugins/رتب.js
@@ -19,7 +19,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 
 ❐↞┇الـوقـت⏳↞ ${(timeout / 1000).toFixed(2)}┇
 ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
-『𝙕𝙊𝙍𝙊-𝘽𝙊𝙏』
+『𝐂𝐋𝐎𝐔𝐃𓆩☁️𓆪𝐊𝐈𝐍𝐆𝐃𝐎𝐌』
 `.trim();
     conn.tekateki[id] = [
        await conn.reply(m.chat, caption, m),
