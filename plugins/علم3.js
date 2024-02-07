8786f2f1ffbf8fa5919d7dc5157b37afc3e1d3d1
Kyutaka101 <142097798+Kyutaka101@users.noreply.github.com>
2024-02-07 15:22:49 +0100

    Add files via upload

---
+++ plugins/علم3.js
@@ -0,0 +1,31 @@
+let timeout = 60000
+let poin = 500
+let handler = async (m, { conn, command, usedPrefix }) => {
+    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
+    let id = m.chat
+    if (id in conn.tebakbendera) {
+        conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
+        throw false
+    }
+    let src = await (await fetch('https://gist.githubusercontent.com/YosefZoro1/10ef5c5023de5b565fbf561adafd86a8/raw/4a97c5abef49be34d1d748faa954b71345d24c3d/game5.json')).json()
+  let json = src[Math.floor(Math.random() * src.length)]
+    let caption = `*${command.toUpperCase()}*
+  ❐↞┇الـوقـت⏳↞ *${(timeout / 1000).toFixed(2)} ┇
+ استخدم .انسحب للأنسحاب
+  ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
+『𝙕𝙊𝙍𝙊-𝘽𝙊𝙏』
+     `.trim()
+    conn.tebakbendera[id] = [
+        await conn.sendFile(m.chat, json.img, '', caption, m),
+        json, poin,
+        setTimeout(() => {
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n❐↞┇الاجـابـة✅↞ ${json.name}*┇`, conn.tebakbendera[id][0])
+            delete conn.tebakbendera[id]
+        }, timeout)
+    ]
+}
+handler.help = ['guessflag']
+handler.tags = ['game']
+handler.command = /^علم/i
+
+export default handler
