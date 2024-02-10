7b4b8141b825f678a147965901abe82273ea2324
Kyutaka101 <142097798+Kyutaka101@users.noreply.github.com>
2024-02-10 22:45:42 +0100

    Update and rename GURU-qrmaker.js to بار_كود.js

--- plugins/بار_كود.js
+++ plugins/بار_كود.js
@@ -5,5 +5,5 @@ conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcod
 }
 handler.help = ['', 'code'].map(v => 'qr' + v + ' <text>')
 handler.tags = ['tools']
-handler.command = /^qr(code)?$/i
+handler.command = ['كود']
 export default handler
