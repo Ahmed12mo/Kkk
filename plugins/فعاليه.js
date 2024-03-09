6fbffb283bca21731ea4205538e753b8ad785c41
Tony stark <142097798+Kyutaka101@users.noreply.github.com>
2024-03-09 16:58:03 +0100

    Update فعاليه.js

--- plugins/فعاليه.js
+++ plugins/فعاليه.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, command, usedPrefix }) => {
         conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
         throw false
     }
-    let src = await (await fetch('https://gist.githubusercontent.com/Kyutaka101/857d6fdd5a0fa738ea00ff578b611c45/raw/894e30b176842aa3fda4f6e0f7f0567c11e3f509/Stark.json')).json()
+    let src = await (await fetch('https://gist.githubusercontent.com/Kyutaka101/2ca0f797f66d524a1f2e3685f66298f2/raw/7fe3898420592b47ca37db07fdd173b9b4ca9a63/Game.json')).json()
   let json = src[Math.floor(Math.random() * src.length)]
     let caption = `*· • • ━━ ⌝🐉⌞ ━━ • • ·*
 *${command.toUpperCase()}*
